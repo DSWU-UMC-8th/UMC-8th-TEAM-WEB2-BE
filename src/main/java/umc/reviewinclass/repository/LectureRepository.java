a55fa50ecee005a050762c20782aa866379a4a95
eldeoddt <eldeoddt@gmail.com>
2025-05-23 20:16:55 +0900

    [FIX] 강의 검색 API 결과 타입을 List<LectureDTO>로 수정 #1

--- src/main/java/umc/reviewinclass/repository/LectureRepository.java
+++ src/main/java/umc/reviewinclass/repository/LectureRepository.java
@@ -3,10 +3,10 @@ package umc.reviewinclass.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.reviewinclass.domain.lecture.Lecture;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
-    Optional<Lecture> findByNameContaining(String name);
+    List<Lecture> findByNameContaining(String name);
 }
 
