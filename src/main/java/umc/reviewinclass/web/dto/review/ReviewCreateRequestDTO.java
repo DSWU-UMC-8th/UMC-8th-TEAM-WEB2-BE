8fca61b604d2f057b9c187a3f325373f11fc2d44
hmj6589 <hmj6589@gmail.com>
2025-05-27 22:39:47 +0900

    [FIX] 강의평 등록 - 원하는 강의 없을 때 강의 등록까지

--- src/main/java/umc/reviewinclass/web/dto/review/ReviewCreateRequestDTO.java
+++ src/main/java/umc/reviewinclass/web/dto/review/ReviewCreateRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import umc.reviewinclass.web.dto.lecture.LectureRequestDTO;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
 public class ReviewCreateRequestDTO {
 
     private Long lectureId;       // 강의명
+    private LectureRequestDTO.createLectureDTO lecture;      // 만약, 원하는 강의 없으면 강의 등록까지 할 수 있도록
     private List<Long> platformIds;      // 플랫폼명
     private Float rating;            // 평점
     private String content;           // 강의평
